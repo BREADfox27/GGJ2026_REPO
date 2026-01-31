52417a89a6567bb2f48e8a71fd5a9c8daa7faf98
BREADfox27 <javierluna271107@gmail.com>
2026-01-31 10:09:53 +0100

    AudioManager

---
+++ GGJ2026/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    [Header("Audio Clip Arrays")]
+    public AudioClip[] musicList;
+    public AudioClip[] sfxList;
+
+    [Header("Audio Source References")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource sfxSource;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayMusic(int musicIndex)
+    {
+        musicSource.clip = musicList[musicIndex];
+        musicSource.Play();
+    }
+
+    public void PLaySFX(int sfxIndex)
+    {
+        sfxSource.PlayOneShot(sfxList[sfxIndex]);
+    }
+}
